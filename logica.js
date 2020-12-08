e4274762816f7a6b9ed58f5d639b0ea1e7205457
Alexamith <samirportillo231099@gmail.com>
2020-12-07 18:33:50 -0600

    SPA

---
+++ logica.js
@@ -0,0 +1,62 @@
+// alert("Hola");
+
+let vista = document.getElementById('vista');
+
+// vista.innerHTML ="<hr> <div class='col-sm-12'>Lorem "+
+// "ipsum dolor sit amet consectetur adipisicing elit. Corrupti nobis culpa dicta nam maxime vitae repellat deleniti libero quis reprehenderit. Corrupti blanditiis nam sit culpa eaque ad soluta voluptatem placeat.         </div>";
+
+function saludar() {
+
+    vista.innerHTML ="<div class='d-flex flex-wrap flex-row-reverse'><div class='card-hotel'><div><h4 class='titulo-hotel negrita'>Hotel San Carlos</h4><p><strong> Descripción:</strong>Hotel ubicado en el centro de La Fortuna de San Carlos</p></div><div><p><strong>Precio:</strong>15,000.00 al día</p></div><div><p><strong>Servicios:</strong> No incluídos</p></div><div> <button class='btn btn-success btn-reserva'>Reservar</button> </div></div>"
+    +"<div class='card-hotel'><div><h4 class='titulo-hotel negrita'>Hotel San Carlos</h4><p><strong> Descripción:</strong>Hotel ubicado en el centro de La Fortuna de San Carlos</p></div><div><p><strong>Precio:</strong>15,000.00 al día</p></div><div><p><strong>Servicios:</strong> No incluídos</p></div><div> <button class='btn btn-success btn-reserva'>Reservar</button> </div></div></div> ";
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
